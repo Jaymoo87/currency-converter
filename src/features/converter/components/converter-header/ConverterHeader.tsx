9bf937be055f269d7e89526bb4ee231e2cd6d6e3
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-04-16 23:38:55 -0500

    Title font size change

--- src/features/converter/components/converter-header/ConverterHeader.tsx
+++ src/features/converter/components/converter-header/ConverterHeader.tsx
@@ -4,10 +4,10 @@ import { Box, Text } from "@chakra-ui/react";
 const ConverterHeader = () => {
   return (
     <Box textAlign="center" color="white" marginTop="20" marginBottom="10">
-      <Text fontWeight="bold" fontSize="3xl" color="#F9ECCC">
+      <Text fontWeight="bolder" fontSize="3xl" color="#F9ECCC">
         Currency Converter
       </Text>
-      <Text fontWeight="light" fontSize="2xl" color="#F9ECCC">
+      <Text fontWeight="bold" fontSize="2xl" color="#F9ECCC">
         Up to date FX rates
       </Text>
     </Box>
